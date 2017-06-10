dd08301522b63465fd8bc388aacd945e68086ce7
blazekotsenburg <u0676062@utah.edu>
2017-06-10 16:40:03 -0600

    Price dropdown
    Price dropdown has been given more interactivity.  This is just to push
    the existing interactivity of the price dropdown, though it is not
    complete.  Needs work for when the min input is focused and making it
    possible to use commas in the input.

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,9 +6,41 @@
  * When max input in Price dropdown is selected, shift all price options to the right of the
  * dropdown column.
  */
+var minPrices = [
+    50000,
+    100000,
+    150000,
+    200000,
+    250000,
+    300000,
+    400000,
+    500000
+];
+
 $("#max-input").on("click", function () {
     $("#price-options").addClass("shift-col-right");
+
+    if(!$("#min-input").val()) {
+        var i = 100000;
+        $(".price").each(function () {
+           $(this).text("$"+ i);
+           i += 100000;
+        });
+    }
+
+    else{
+        var j = 25000;
+
+        if($.isNumeric($("#min-input").val())){
+            $(".price").each(function () {
+                var valToInt = parseInt($("#min-input").val(), 10);
+                $(this).text("$" + (valToInt+ j));
+                j += 25000;
+            })
+        }
+    }
 });
+
 
 /**
  * When min input in Price dropdown is selected, shift all price options to the left of the
@@ -18,8 +50,7 @@ $("#min-input").on("click", function () {
     $("#price-options").removeClass("shift-col-right");
 });
 
-$(".dropdown").on("mouse", function () {
-    $(this).css({
-       backgroundColor: "#080808"
-    });
+$(".dropdown").mouseenter(function () {
+    $("#min-input").focus();
+    $("#price-options").removeClass("shift-col-right");
 });

4d664828a61df99a25bad299cce80ec1b6ecf71e
blazekotsenburg <u0676062@utah.edu>
2017-06-29 16:40:36 -0600

    Small updates to listing grid
    Cleaning up selection and picking image for when no image condition
    appears.

--- assets/app.js
+++ assets/app.js
@@ -9,13 +9,15 @@ var express          = require("express"),
 
 var url              = 'https://secure.realcove.com/api.php?';
 
-var data = {
+var defaultData = {
     partner_key      : '7e52cad4e91ee36e308d35f93a9db02b',     //International MLS
     action           : 'propertySearch',                      //agentSearch, officeSearch, propertySearch, pickListSearch
     return           : 'json', 							     //xml, json
     search_offset    : '0',
-    search_limit     : '15',						         //MAX BATCH IS 100, OVER THAT DEFAULTS TO 15
+    search_limit     : '100',						         //MAX BATCH IS 100, OVER THAT DEFAULTS TO 15
     search_mls_id    : ['1'],                                 //Array of valid MLS id's, Park City MLS = 1 and WFRMLS = 2
+    //search_price_min : '800000',
+    //search_price_max : '1500000',
     debug            : '0'
 };
 
@@ -24,7 +26,7 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
 
-    request.post({url: url, formData: data}, function(err, httpResponse, body) {
+    request.post({url: url, formData: defaultData}, function(err, httpResponse, body) {
         if (!err && httpResponse.statusCode == 200) {
             var mlsData = JSON.parse(body);
             res.render('home', {mlsData: mlsData['data']});
@@ -33,9 +35,6 @@ app.get("/", function (req, res) {
             return console.error('upload failed:', err); //possibly send to error page with error message and status code??
         }
     });
-
-    //res.render("home");
-
 });
 
 app.get("/home", function (req, res) {

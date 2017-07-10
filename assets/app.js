03c218ab0252534521ce824fc85ba13ecf1361f2
blazekotsenburg <u0676062@utah.edu>
2017-07-10 01:42:10 -0600

    Current version
    This is my most up to date Version

--- assets/app.js
+++ assets/app.js
@@ -148,10 +148,10 @@ var defaultData = {
     action           : 'propertySearch',                      //agentSearch, officeSearch, propertySearch, pickListSearch
     return           : 'json', 							     //xml, json
     search_offset    : '0',
-    search_limit     : '100',						         //MAX BATCH IS 100, OVER THAT DEFAULTS TO 15
+    search_limit     : '15',						         //MAX BATCH IS 100, OVER THAT DEFAULTS TO 15
     search_mls_id    : ['1'],                                 //Array of valid MLS id's, Park City MLS = 1 and WFRMLS = 2
-    //search_price_min : '800000',
-    //search_price_max : '1500000',
+    search_price_min : '800000',
+    search_price_max : '1500000',
     debug            : '0'
 };
 
@@ -163,7 +163,6 @@ app.get("/", function (req, res) {
     request.post({url: url, formData: defaultData}, function(err, httpResponse, body) {
         if (!err && httpResponse.statusCode == 200) {
             var mlsData = JSON.parse(body);
-            //console.log(mlsData);
             res.render('home', {mlsData: mlsData['data']});
         }
         else {
@@ -178,21 +177,37 @@ app.get("/home", function (req, res) {
 
 app.get("/listing", function (req, res) {
 
-    //This will need to receive data from another page and use it to form a new listing grid
-    // and still be able to pull the correct requested listing from RealCove.
-    request.post({url: url, formData: defaultData}, function(err, httpResponse, body) {
+    var listData = {
+        list_id         : req.query.id,
+        address         : req.query.address,
+        longitude       : req.query.long,
+        latitude        : req.query.lat,
+        area            : req.query.area
+    };
+
+    var data = {
+        partner_key        : '7e52cad4e91ee36e308d35f93a9db02b',
+        action             : 'propertySearch',
+        return             : 'json',
+        search_offset      : '0',
+        search_limit       : '15',
+        search_mls_id      : ['1'],
+        //search_area_name : req.query.area,
+        debug              : '0'
+    };
+
+    request.post({url: url, formData: data}, function(err, httpResponse, body) {
         if (!err && httpResponse.statusCode == 200) {
             var mlsData = JSON.parse(body);
-            //console.log(mlsData);
-            res.render('listing', {mlsData: mlsData['data']});
+
+            res.render('listing', {mlsData: mlsData['data'], listData: listData});
         }
         else {
-            return console.error('upload failed:', err); //possibly send to error page with error message and status code??
+            return console.error('upload failed:', err);
         }
     });
 });
 
-//Trying to pull all of the data once this port has been requested
 app.listen(8000, "localhost", function () {
     console.log("Server is running...");
 });
